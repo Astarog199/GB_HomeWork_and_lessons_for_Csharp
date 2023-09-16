92ee47ef891130b024242d3adbe0168b3688bc02
pkn <PKN199@yandex.ru>
2023-09-16 13:56:08 +0300

    lesson2:introduction to functions and arrays

---
+++ les2.2/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = new {10, 24, 44, 33};
+int n = array.Length;
+int i = 0;
+int max = 0;
+
+while (i < n)
+{
+    if(array[i] > max)
+    {
+        max = array[i];
+    }
+    i++;  
+}
+ Console.Write(max);
