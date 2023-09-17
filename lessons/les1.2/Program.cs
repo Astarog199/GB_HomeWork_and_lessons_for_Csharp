64d5b09ee4b17202d1b02ad66a769d8fa53f54ac
pkn <PKN199@yandex.ru>
2023-09-17 13:22:10 +0300

    update

---
+++ lessons/les1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿// cложение чисел
+int numberA = new Random().Next(1, 12); // случайные числа от 1 до 12
+int numberB = new Random().Next(1, 12); // случайные числа от 1 до 12
+Console.Write("Число numberA = ");
+Console.Write(numberA);
+Console.Write(", число numberB = ");
+Console.WriteLine(numberB);
+int result_sum = numberA + numberB;
+Console.WriteLine(result_sum);
+
+//деление чисел
+double numberC = 12;
+double numberD = 5;
+double result_div = numberC / numberD;
+Console.WriteLine(result_div);
+
+/*
+ключевое слово: string используется для строк
+ключевое слово: int используется для целых чисел
+ключевое слово: double используется для дробных чисел
+*/
---
+++ lessons/les3.1/Program.cs
@@ -0,0 +1,37 @@
+﻿// 1 мектод
+// void Method1()
+// {
+//     Console.WriteLine("тест");
+// }
+// Method1();
+
+// 2 метод
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2("тест");
+
+// 3 метод
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// Console.WriteLine(year);
+
+// 4 метод
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty; // создаём пустую строку
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
