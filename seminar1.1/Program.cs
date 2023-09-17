dbf9c89893706918411e420cc761abfca6280d86
pkn <PKN199@yandex.ru>
2023-09-17 13:28:19 +0300

    update

--- seminar1.1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿//принмаем трёх значное число и на выходе показываем последнюю цифру этого числа
-
-int i = new Random().Next(100, 999);
-//int i = -345;
-Console.Write($"last character of the number {i}: ");
-char[] arr = i.ToString().ToCharArray();
-
-int position = arr.Length;
-Console.WriteLine(arr[position - 1]);
