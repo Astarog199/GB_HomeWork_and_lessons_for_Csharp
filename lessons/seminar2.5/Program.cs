5bcdfec7e4089e2ce85dbcbe1124091f6f52ef30
pkn <PKN199@yandex.ru>
2023-09-20 08:09:43 +0300

    проверка на кратность 2

---
+++ lessons/seminar2.5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на
+// вход два числа и выводит, является ли второе число кратным первому.
+// Если число 2 не кратно числу 1, то программа выводит остаток
+
+void Check(int num1, int num2)
+{
+    if (num2 % num1 == 0)
+        Console.WriteLine("True");
+    else
+        Console.WriteLine($"false {num2 % num1}");
+}
+
+Console.WriteLine($"Your number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Your number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == 0)
+    Console.WriteLine("Error");
+else
+    Check(num1, num2);
