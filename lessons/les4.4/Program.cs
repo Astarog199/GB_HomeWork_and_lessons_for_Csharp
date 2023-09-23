ee383024b62efafc942b1db9da02d0368844e144
pkn <PKN199@yandex.ru>
2023-09-23 16:30:13 +0300

    Рекурсия:Программа вычисляет факториал числал

---
+++ lessons/les4.4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа вычисляет факториал числа
+double Factorial (int n)
+{
+    // 1! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+for (int i =1; i<=20; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+// 19! = 1,21645100408832E+17 (число у множенное на 10(17) десять в семнадцатой степени)
