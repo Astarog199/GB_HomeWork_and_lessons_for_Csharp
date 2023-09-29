f55a1da89b0a700a74edab740c1d49f97d275386
pkn <PKN199@yandex.ru>
2023-09-29 06:43:49 +0300

    произведение чисел от 1 до N

---
+++ lessons/seminar4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая
+принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+int number = 5;
+
+void func(int number)
+{
+    int res = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        res *= i;
+    }
+
+    Console.WriteLine($"product of {number} from 1 is {res}.");
+}
+func(number);
