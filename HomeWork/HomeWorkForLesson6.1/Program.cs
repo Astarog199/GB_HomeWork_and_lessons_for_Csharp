f5e706232c781c06269bf258b6276f8fb6c5754f
pkn <PKN199@yandex.ru>
2023-10-07 09:56:08 +0300

    Home work for lesson 6

---
+++ HomeWork/HomeWorkForLesson6.1/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+// Создание массива и заполнение его
+
+int[] FillArray(int size)
+
+{
+    int[] array = new int[size];
+
+    for (int index = 0; index < size; index++)
+    {
+        array[index] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+
+    Console.Write($"Массив из {size} элементов -> [");
+
+    for (int position = 0; position < size; position++)
+    {
+        Console.Write($"{array[position]}");
+        if (array.Length > position + 1)
+        {
+            Console.Write(", ");
+        }
+    }
+
+    Console.WriteLine("]");
+}
+
+void Calc(int[] array)
+{
+    int res = 0;
+    for (int position = 0; position < array.Length; position++)
+    {
+        if (array[position] > 0)
+        {
+            res++;
+        }
+    }
+    Console.WriteLine($"quantity is more than 0-> {res}");
+}
+
+Console.Write("Введите число элементов: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = FillArray(size);
+PrintArray(array);
+Calc(array);
---
+++ HomeWork/HomeWorkForLesson6.2/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+
+// Создание массива и заполнение его
+
+using System.Runtime.Intrinsics.X86;
+
+int[] FillArray(int b1, int k1, int b2, int k2)
+
+{
+    int[] array = new int[4];
+
+    array[0] = b1;
+    array[1] = k1;
+    array[2] = b2;
+    array[3] = k2;
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"b1 = {array[0]}");
+    Console.WriteLine($"k1 = {array[1]}");
+    Console.WriteLine($"b2 = {array[2]}");
+    Console.WriteLine($"b3 = {array[3]}");
+}
+
+
+void Calc(int[] array)
+{
+    double x1 = array[2] - array[0];
+    double x2 = array[1] - array[3];
+    double x = x1 / x2;
+
+    double res = array[1] * x + array[0];
+    double res2 = array[3] * x + array[2];
+
+    Console.WriteLine($"Point of intersection of two lines-> {res}, {res2}");
+}
+
+
+Console.WriteLine("Set values for b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Set values for k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Set values for b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Set values for k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+int[] array = FillArray(b1, k1, b2, k2);
+PrintArray(array);
+Calc(array);
