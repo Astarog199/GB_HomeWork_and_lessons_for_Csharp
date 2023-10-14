60b318e2cfa4341074bb40068e01cff425db5e3d
pkn <PKN199@yandex.ru>
2023-10-14 09:48:56 +0300

    урок 4. функции

---
+++ lessons/seminar4/seminar4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 26: Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5 */
+
+int num = 89126;
+int number = num;
+void func(int num, int number)
+{
+    int num2 = 0;
+    while (num > 1)
+    {
+        num = num / 10;
+        num2++;
+    }
+    Console.WriteLine($"the number of digits in a {number} is {num2}.");
+}
+func(num, number);
---
+++ lessons/seminar4/seminar4.3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Находит все нечётные числа от 1 до N
+
+Console.WriteLine($"Введите число.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void func(int number)
+{
+    for (int i = 1; number >= i; i++)
+    {
+        if (i % 2 == 1)
+            Console.WriteLine($"{i} нечётное число.");
+        else
+            Console.WriteLine($"{i} чётное число.");
+    }
+}
+
+func(number);
