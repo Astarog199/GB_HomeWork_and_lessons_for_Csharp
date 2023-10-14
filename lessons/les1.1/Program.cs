de14d4a2cbaddcaffac66e1bf32cb7a1e399c12b
pkn <PKN199@yandex.ru>
2023-10-14 14:51:39 +0300

    update

--- lessons/les1.1/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// Урок 1. Знакомство с языком программирования C#
-// приветствие пользователя
-
-Console.WriteLine("Введите ваше имя");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
--- lessons/les3.2/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿for (int i = 2; i <= 5; i++)
-{
-    for (int j = 2; j <= 5; j++)
-    {
-        Console.WriteLine($"{i} x {j} = {i * j}");
-    }
-}
