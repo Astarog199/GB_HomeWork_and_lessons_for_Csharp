079354dd280284f21dd92777fe649d6cb7b91417
pkn <PKN199@yandex.ru>
2023-10-14 14:50:17 +0300

    урок 8. двумерные массивы. продолжение

---
+++ lessons/seminar8/seminar8.3/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+Заменяем строки на столбцы, а строки на столбцы в массиве.
+*/
+
+
+int[,] Create2dArray(int min, int max, int NumberOfRows, int NumberOfColumns)
+{
+    int[,] array = new int[NumberOfRows, NumberOfColumns];
+    for (int row = 0; row < NumberOfRows; row++)
+    {
+        for (int column = 0; column < NumberOfColumns; column++)
+        {
+            array[row, column] = new Random().Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int[,] matrix, string message = "")
+{
+    Console.WriteLine();
+
+    Console.WriteLine(message);
+    int rowsCount = matrix.GetLength(0);
+    int colsCount = matrix.GetLength(1);
+    Console.Write("\t");
+    for (int j = 0; j < colsCount; j++)
+    {
+        Console.Write($"col{j}\t");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+    for (int i = 0; i < rowsCount; i++)
+    {
+        Console.Write($"row{i}:\t");
+        for (int j = 0; j < colsCount; j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] array = Create2dArray(0, 9, 3, 3);
+Show2dArray(array, "Отображаем созданный массив");
+
+
+int[,] change2dArr(int[,] array)
+{
+
+    int Rows = array.GetLength(0); //получаем число строк
+    int Columns = array.GetLength(1); // получаем число столбцов
+
+    if (Rows != Columns)
+    {
+        Console.WriteLine("ошибка: Количество строк и столбцов должно быть одинаковым");
+        return array;
+    }
+
+    int column2 = 0;
+    int row2;
+
+    int[,] array2 = new int[Rows, Columns];
+
+    for (int row = 0; row < Rows; row++)
+    {
+        row2 = 0;
+        for (int column = 0; column < Columns; column++)
+        {
+            array2[row2, column2] = array[row, column];
+            row2++;
+        }
+        column2++;
+    }
+    return array2;
+}
+
+
+int[,] newArr = change2dArr(array);
+Show2dArray(newArr, "Меняем местами строки и столбцы в массиве");
---
+++ lessons/seminar8/seminar8.4/Program.cs
@@ -0,0 +1,116 @@
+﻿/*
+Составить частотный словарь элементов двумерного массива. 
+Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+*/
+
+
+int[,] Create2dArray(int min, int max, int NumberOfRows, int NumberOfColumns)
+{
+    int[,] array = new int[NumberOfRows, NumberOfColumns];
+    for (int row = 0; row < NumberOfRows; row++)
+    {
+        for (int column = 0; column < NumberOfColumns; column++)
+        {
+            array[row, column] = new Random().Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int[,] matrix, string message = "")
+{
+    Console.WriteLine();
+
+    Console.WriteLine(message);
+    int rowsCount = matrix.GetLength(0);
+    int colsCount = matrix.GetLength(1);
+    Console.Write("\t");
+    for (int j = 0; j < colsCount; j++)
+    {
+        Console.Write($"col{j}\t");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+    for (int i = 0; i < rowsCount; i++)
+    {
+        Console.Write($"row{i}:\t");
+        for (int j = 0; j < colsCount; j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] array = Create2dArray(0, 9, 3, 3);
+Show2dArray(array, "Отображаем созданный массив");
+
+
+
+int[] getElements(int[,] matrix)
+{
+    int rowsCount = matrix.GetLength(0);
+    int colsCount = matrix.GetLength(1);
+    int size = rowsCount * colsCount;
+    int index = 0;
+
+    int[] container = new int[size]; // инициализация массива
+    for (int row = 0; row < rowsCount; row++)
+    {
+        for (int column = 0; column < colsCount; column++)
+        {
+            container[index] = array[row, column];
+            index++;
+        }
+    }
+    return container;
+}
+
+int[] container = getElements(array);
+
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+
+    Console.Write($"container -> [");
+
+    for (int position = 0; position < size; position++)
+    {
+        Console.Write($"{array[position]} ");
+    }
+    Console.WriteLine("]");
+}
+
+PrintArray(container);
+
+
+
+void arrayCheck(int[,] matrix, int[] container)
+{
+    int size = container.Length;
+
+    int rowsCount = matrix.GetLength(0);
+    int colsCount = matrix.GetLength(1);
+
+    for (int row = 0; row < rowsCount; row++)
+    {
+
+
+        for (int column = 0; column < colsCount; column++)
+        {
+
+            int count = 0;
+            for (int index = 0; index < size; index++)
+            {
+                if (array[row, column] == container[index])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"элемент двумерного массива {array[row, column]} повторяется {count} раз");
+        }
+    }
+}
+
+arrayCheck(array, container);
