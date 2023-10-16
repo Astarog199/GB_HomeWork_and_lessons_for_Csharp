38dd31d574db8bd6e846ed66449d24bccfc9fc23
pkn <PKN199@yandex.ru>
2023-10-16 21:46:10 +0300

    урок 9. рекурсия

---
+++ lessons/seminar9/seminar9.1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Факториал числа n
+
+int Fact(int n)
+{
+    if (n > 1)
+    {
+        return Fact(n - 1) * n;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int res = Fact(5);
+Console.Write(res);
+
+/*
+n=5;
+(5-1) * 5 = 20;
+(4-1) * 20 = 60; 
+(3-1) * 60 = 120;
+(2-1) * 120 = 120;
+(1>1) => false;
+Fact = 120;
+
+Fact(1) => false;
+Fact(2) * 120 = 120;
+Fact(3) * 60 = 120;
+Fact(4) * 20 = 60;
+Fact(5) * 5 = 20;
+*/
---
+++ lessons/seminar9/seminar9.2/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+
+*/
+
+
+Console.Write($"Enter the number m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Enter the number n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+int ShoWValues(int m, int n)
+{
+    if (/* Math.Min(m, n) < Math.Max(m, n) */ m <= n)
+    {
+
+        Console.WriteLine($"n: {n} ");
+        return ShoWValues(m, n - 1) + n;
+    }
+    else
+    {
+        return 0;
+    }
+
+}
+
+int result = ShoWValues(m, n);
+
+Console.Write(result);
+
